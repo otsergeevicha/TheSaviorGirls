886160ee86dba021fec41876238051b73efa3cf2
Alexandr Ivanov <otsergeevicha@gmail.com>
2023-08-09 21:15:49 +0300

    Has been fix bug dependency inject  on load level

--- Assets/Scripts/Reflex/MainSceneInstaller.cs
+++ Assets/Scripts/Reflex/MainSceneInstaller.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using BuildLogic;
+﻿using BuildLogic;
 using Infrastructure.Factory.Pools;
 using Plugins.MonoCache;
 using Reflex.Core;
@@ -15,26 +14,38 @@ namespace Reflex
 {
     public class MainSceneInstaller : MonoCache, IInstaller
     {
+        private ISave _save;
+        private IInputService _input;
+        private IWallet _wallet;
+        private IGameFactory _gameFactory;
+        
+        private Pool _pool;
+        private MainPlatform _mainPlatform;
+        private TowerBuilder _towerBuilder;
+        private Tank _tank;
+        private ObstaclePattern _obstaclePattern;
+
         public void InstallBindings(ContainerDescriptor descriptor) => 
             descriptor.OnContainerBuilt += LoadLevel;
 
         private void LoadLevel(Container container)
         {
-            ISave save = container.Single<ISave>();
-            IInputService input = container.Single<IInputService>();
-            IWallet wallet = container.Single<IWallet>();
-            IGameFactory gameFactory = container.Single<IGameFactory>();
-
-            Pool pool = gameFactory.CreatePool();
-            pool.Construct(gameFactory);
+             _save = container.Single<ISave>();
+            _input = container.Single<IInputService>();
+            _wallet = container.Single<IWallet>();
+            _gameFactory = container.Single<IGameFactory>();
+        
+            _pool = _gameFactory.CreatePool();
+            _pool.Construct(_gameFactory);
             
-            MainPlatform mainPlatform = gameFactory.CreateMainPlatform();
-            mainPlatform.Construct(new TowerBuilder(pool, mainPlatform.transform, Constants.TowerSize), pool);
-
-            ObstaclePattern obstaclePattern = gameFactory.CreateObstaclePattern();
+            _mainPlatform = _gameFactory.CreateMainPlatform();
+            _towerBuilder = new TowerBuilder(_pool, _mainPlatform.transform, Constants.TowerSize);
+            _mainPlatform.Construct(_towerBuilder, _pool);
+        
+            _obstaclePattern = _gameFactory.CreateObstaclePattern();
             
-            Tank tank = gameFactory.CreateTank();
-            tank.Construct(mainPlatform.GetSpawnPointTank, pool, input);
+            _tank = _gameFactory.CreateTank();
+            _tank.Construct(_mainPlatform.GetSpawnPointTank, _pool, _input);
         }
     }
 }
